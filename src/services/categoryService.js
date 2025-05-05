c2d976af342c0cd1fd26f24a9bbe8190f72edc15
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2025-05-05 13:29:31 +0300

    categories ordered by usage

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,11 +1,11 @@
 // import axios from "axios";
 import API from "./api";
 
-const API_URL = "https://localhost:7271/api/Category"; // Замени с твоя API URL
+const API_URL = "https://localhost:7271/api/Category";
 
 export async function getCategories() {
   try {
-    const response = await API.get(API_URL);
+    const response = await API.get(API_URL + "/expenses/ordered");
     return response.data;
   } catch (error) {
     console.error("Error fetching categories:", error);
